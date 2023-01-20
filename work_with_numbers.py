6cbcf86fc1709381b5c88a86be08c23ada013eec
John Otilla <johncanila.otilla@bicol-u.edu.ph>
2023-01-20 14:11:01 +0800

    Dealing with number in Python

---
+++ work_with_numbers.py
@@ -0,0 +1,31 @@
+# Working with Numbers
+
+from math import *
+
+#Simple printing a number
+print(2.9)
+print(-23.34)
+print(34535)
+
+#Simple Arithmetic
+print(1 + 2)
+print(3 * (4 + 6))
+print(6 % 3) # using modulo
+
+# Store in a variable
+num = 24
+
+print(num) #print num
+print(str(num) + " is a string") # Convert num into a string
+
+# Some function in dealing with number in Python
+num = 7
+
+print(abs(4)) # Getting the absolute number
+print(pow(num, 2)) #-4^2 Getting the square of a number
+print(max(num, 5)) # Getting the maximum number
+print(min(num, 5)) # Getting the minimum number
+print(round(7.5)) # Rounding up to the nearest number
+print(floor(3.7)) # Floor fucntion
+print(ceil(3.5)) # Ceil function
+print(sqrt(14)) # Get the square root of a number
